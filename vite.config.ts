7cce4c506c0469b867e5019be038a9912a0e16f4
Minh Trần <diobrando1403@gmail.com>
2025-05-31 10:27:36 +0700

    fix ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,14 +6,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: './src/main.jsx', // Entry point of your app
-      name: 'MyReactApp', // Global variable name for UMD
-      fileName: 'my-react-app', // Output file name (my-react-app.js)
-      formats: ['umd'], // UMD format for script tag compatibility
+      entry: './src/main.tsx',
+      name: 'MyReactApp',
+      fileName: 'my-react-app',
+      formats: ['umd'],
     },
     rollupOptions: {
-      // Externalize React and ReactDOM if you want to load them separately
-      // external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
